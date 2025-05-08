ae8ae233e5c41fd5d834b09611c6594117869934
rywilliams-webdev <97313288+rywilliams-webdev@users.noreply.github.com>
2025-05-08 15:51:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/vite-deploy-demo/',
+  base: '/ryan-recipes/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
