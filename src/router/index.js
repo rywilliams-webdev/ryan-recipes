397e15b9aee378a7ae2c430d5f4344eebbfe8648
rywilliams-webdev <rywilliams.webdev@gmail.com>
2025-05-12 11:56:26 -0300

    update app name

--- src/router/index.js
+++ src/router/index.js
@@ -14,14 +14,26 @@ const router = createRouter({
         page: route.query.page,
         cuisine: route.query.cuisine,
       }),
+      meta: {
+        title: 'Home', // Default title
+      },
     },
     {
       path: '/recipe-details/:id',
       name: 'recipe-details',
       component: RecipeDetailsView,
       props: true,
+      meta: {
+        title: 'Recipe Details', // Default title
+      },
     },
   ],
+})
+
+// Update document title dynamically
+router.afterEach((to) => {
+  const defaultTitle = 'Ryan Recipes'
+  document.title = defaultTitle + ' | ' + to.meta.title
 })
 
 export default router
